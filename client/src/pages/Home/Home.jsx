78f8b423cbebcd6ce78dc91adbf093e392b28d1c
Margaritkins <te120883ama@gmail.com>
2024-03-19 22:55:23 +0100

    Create HomeImages component &  remove from Home not used code

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -10,6 +10,7 @@ import carouselConstants from '../../carouselConstants';
 import Spinner from '../../components/Spinner/Spinner';
 import WhySquadhelp from '../../components/homeComponents/WhySquadhelp';
 import HowNameContestWork from '../../components/homeComponents/HowNameContestWork';
+import HomeImages from '../../components/homeComponents/HomeImages';
 
 const Home = (props) => {
   const [index, setIndex] = useState(0);
@@ -63,131 +64,9 @@ const Home = (props) => {
               />
             </div>
             <WhySquadhelp />
-            <div className={styles.greyContainer}>
-              <div className={styles.adv}>
-                <div className={styles.images}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/Forbes-inactive.png`}
-                    alt="forbes"
-                  />
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/Forbes-active.png`}
-                    alt="forbes"
-                  />
-                </div>
-                <div className={styles.images}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/the_next_web_inactive.png`}
-                    alt="web"
-                  />
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/the_next_web_active.png`}
-                    alt="web"
-                  />
-                </div>
-                <div className={styles.images}>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/mashable-inactive.png`}
-                    alt="mashable"
-                  />
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}sponsors/mashable-active.png`}
-                    alt="mashable"
-                  />
-                </div>
-              </div>
-              <div className={styles.stats}>
-                <div>
-                  <p>119,525</p>
-                  <span>Creatives</span>
-                </div>
-                <div>
-                  <p>21,875</p>
-                  <span>Customers</span>
-                </div>
-                <div>
-                  <p>85</p>
-                  <span>Industries</span>
-                </div>
-              </div>
-            </div>
+            <HomeImages />
             <h2>How Do Name Contest Work?</h2>
-            {/* <div className={styles.whiteContainer}> */}
               <HowNameContestWork />
-              {/* <div className={styles.stepReverse}>
-                <div>
-                  <h3>Step 1: Launch a Naming Contest</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Start your project right with our proven Naming Brief
-                      template
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      We’ll walk you through exactly what you need to share
-                      about your project in order to get an awesome Name
-                    </span>
-                  </p>
-                </div>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/1-compressed.gif`}
-                  alt="compressed"
-                />
-              </div> */}
-            {/* </div> */}
-            {/* <div className={styles.greenContainer}>
-              <div className={styles.step}>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/2-compressed-new.gif`}
-                  alt="compressed"
-                />
-                <div className={styles.greenStep}>
-                  <h3>Step 2: Ideas start pouring in within minutes</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      100s of naming experts start submitting name ideas
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Names automatically checked for URL availability
-                    </span>
-                  </p>
-                </div>
-              </div>
-            </div> */}
-            {/* <div className={styles.greyContainer}>
-              <div className={styles.stepReverse}>
-                <div>
-                  <h3>Step 3: Rate Entries & Brainstorm with Creatives</h3>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>Provide instant feedback on Names</span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      Send private feedback or public messages to all creatives
-                    </span>
-                  </p>
-                  <p>
-                    <i className="fas fa-check" />
-                    <span>
-                      The more entries you rate - the submissions get better and better
-                    </span>
-                  </p>
-                </div>
-                <img
-                  src={`${CONSTANTS.STATIC_IMAGES_PATH}gif/3-compressed.gif`}
-                  alt="compressed"
-                />
-              </div>
-            </div> */}
             <div className={styles.headerBar}>
               <h3>Names For Sale</h3>
               <p className={styles.blueUnderline}>
