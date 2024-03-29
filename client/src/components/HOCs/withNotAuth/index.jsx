8a1c80493cb602fe4b8b27cdf3b8f10e73e30f3b
Margaritkins <te120883ama@gmail.com>
2024-03-29 20:19:20 +0100

    Change HOCs to function component

--- client/src/components/HOCs/withNotAuth/index.jsx
+++ client/src/components/HOCs/withNotAuth/index.jsx
@@ -1,32 +1,56 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUser } from '../../../store/slices/userSlice';
 import Spinner from '../../Spinner/Spinner';
 
-const withNotAuth = Component => {
-  class HocForLoginSignUp extends React.Component {
-    componentDidMount () {
-      this.props.checkAuth(this.props.history.replace);
+const withNotAuth = (Component) => {
+  const HocForLoginSignUp = (props) => {
+    const { history } = props;
+
+    const { isFetching, data } = useSelector((state) => state.userStore);
+
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+      dispatch(getUser(history.replace)); // eslint-disable-next-line
+    }, []);
+
+    if (isFetching) {
+      return <Spinner mtop />;
     }
-
-    render () {
-      if (this.props.isFetching) {
-        return <Spinner mtop />;
-      }
-      if (!this.props.data) {
-        return <Component history={this.props.history} />;
-      }
-      return null;
+    if (!data) {
+      return <Component history={history} />;
     }
-  }
+    return null;
+  };
 
-  const mapStateToProps = state => state.userStore;
-
-  const mapDispatchToProps = dispatch => ({
-    checkAuth: replace => dispatch(getUser(replace)),
-  });
-
-  return connect(mapStateToProps, mapDispatchToProps)(HocForLoginSignUp);
+  return HocForLoginSignUp;
 };
+
+// const withNotAuth = Component => {
+//   class HocForLoginSignUp extends React.Component {
+//     componentDidMount () {
+//       this.props.checkAuth(this.props.history.replace);
+//     }
+
+//     render () {
+//       if (this.props.isFetching) {
+//         return <Spinner mtop />;
+//       }
+//       if (!this.props.data) {
+//         return <Component history={this.props.history} />;
+//       }
+//       return null;
+//     }
+//   }
+
+//   const mapStateToProps = state => state.userStore;
+
+//   const mapDispatchToProps = dispatch => ({
+//     checkAuth: replace => dispatch(getUser(replace)),
+//   });
+
+//   return connect(mapStateToProps, mapDispatchToProps)(HocForLoginSignUp);
+// };
 
 export default withNotAuth;
